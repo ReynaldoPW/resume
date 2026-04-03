f4aed4e87624875a814a7264550d212f2966b495
ReynaldoPW <reynaldopaulini@yahoo.com>
2026-04-03 07:55:12 +0700

    Enhance dark mode functionality and typing effect
    Added support for system dark mode preference and improved dark mode toggle functionality.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 document.getElementById("darkToggle").onclick = () => {
   document.body.classList.toggle("dark");
 };
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('dark');
+}
 
 // TYPING EFFECT
 const text = "QA Engineer | Automation Tester | API Testing";
